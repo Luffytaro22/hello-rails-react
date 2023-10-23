eaf8995876b99f4b206965d783750cd633254e1d
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-23 12:28:06 -0500

    Create the logic to select a random greeting

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::GreetingsController < ApplicationController
+	before_action :find_greetings
+	def index
+		@random = rand(0..4)
+		@greeting = @greetings[@random]
+		render json: @greeting
+	end
+
+	private
+
+	def find_greetings
+		@greetings = Greeting.all
+	end
 end
