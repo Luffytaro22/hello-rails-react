d1abd0b253cc86ebca1f2d208fd6408f9455d339
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-23 12:18:09 -0500

    Add the api routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "greetings#index"
   # root "posts#index"
+
+  #API routes
+    namespace :api do
+      namespace :v1 do
+        get "greetings#index"
+      end
+   	end
 end
