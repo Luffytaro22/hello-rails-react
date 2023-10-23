d6d18b5a08d22e85c55bea73d4e3b0b372829534
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-23 16:29:34 -0500

    Dispatch the getGreeting function

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,11 +3,17 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import { Provider } from 'react-redux'
+import { Provider } from 'react-redux'
 import App from './components/app'
+import store from './redux/store'
+import { getGreeting } from './redux/greetings/greetingSlice'
+
+store.dispatch(getGreeting());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>,
 )
